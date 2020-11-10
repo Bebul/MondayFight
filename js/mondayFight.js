de2bc94436b7e7b6cc8fb28e210c4aa8c9d4e2ba
Bebul <Bebul@users.noreply.github.com>
2020-11-10 14:11:34 +0100

    download missing tournaments in admin mode

--- js/mondayFight.js
+++ js/mondayFight.js
@@ -246,7 +246,7 @@ class LichessAPI {
   constructor() {
     this.busy = false;
     this.current = undefined;
-    this.requests = [];  // array of xhttp requests waiting to send
+    this.requests = [];  // array of requests waiting to process
   }
   // xhttp should be already opened, we can make only one request per time we also do at most one request per second
   addHttpRequest(xhttp, url) {
@@ -312,27 +312,22 @@ function getDataJSON() {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // main initialization
+
+let urlRequestsList = []
+
 function init() {
 
   // nice is: https://developers.google.com/web/updates/2015/03/introduction-to-fetch
   //textFile2String('pgn/parsePgn.js');
 
   let allFights = jouzoleanAndBebulsTournaments;
-  allFights.sort(function(a,b){
-    let x = a.startsAt;
-    let y = b.startsAt;
-    if (x < y) {return -1;}
-    if (x > y) {return 1;}
-    return 0;
-  });
 
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const admin = urlParams.get('bebul')!=null
 
-  if (!admin) {
-    document.getElementById("adminStuff").hidden = true;
-    document.getElementById("adminStuff2").hidden = true;
+  if (admin) {
+    document.getElementById("adminStuff").style.display = "block";
   }
 
   let text = "<table><th>Url</th><th>Players</th><th>Games</th><th>Date</th><th>Gold</th><th>Score</th><th>ELO</th><th>Silver</th><th>Score</th><th>ELO</th><th>Bronze</th><th>Score</th><th>ELO</th>";
@@ -360,8 +355,129 @@ function init() {
   text += "</table>";
   if (admin) {
     document.getElementById("demo").innerHTML = text;
-    allFights.forEach(downloadPerformances)
+
+    downloadMissingTournaments(allFights, ["bebul","Jouzolean"])
   }
+
+  function containsId(fights, id) {
+    let ret = false
+    fights.forEach(fight => {
+      if (fight.id===id) {
+        ret = true;
+        return
+      }
+    })
+    return ret
+  }
+
+  function updateHTMLurlRequestsList(url) {
+    urlRequestsList.push(url)
+    let txt = ""
+    urlRequestsList.forEach(url => {
+      txt = txt + url + "\n"
+    })
+    document.getElementById("updated").innerHTML = txt;
+  }
+
+
+  async function downloadMissingTournaments(fights, users) {
+    let timeout = 5000
+    const promiseTimeout = time => result => new Promise(resolve => setTimeout(resolve, time, result));
+
+    function status(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return Promise.resolve(response)
+      } else {
+        return Promise.reject(new Error(response.statusText))
+      }
+    }
+    function text(response) {
+      return response.text()
+    }
+    function ndjson2array(text) {
+      let json = "[" + text.replace(/\n{/g, ",{") + "]";
+      return json
+    }
+    function json(response) {
+      return response.json()
+    }
+    function parse(response) {
+      return JSON.parse(response)
+    }
+    function getMissingTournaments(tournaments) {
+      let missing = []
+      tournaments.forEach(tr => {
+        if (!containsId(fights, tr.id)) missing.push(tr.id)
+      })
+      return missing
+    }
+    async function downloadTournament(id) {
+      let url = "https://lichess.org/api/tournament/" + id
+      updateHTMLurlRequestsList(url)
+      let theTournament = await fetch(url)
+        .then(status)
+        .then(text)
+        .then(parse)
+
+      let partial = theTournament
+      while (partial.nbPlayers / 10 > partial.standing.page) {
+        let url2 = "https://lichess.org/api/tournament/" + id + "?page=" + (partial.standing.page + 1)
+        updateHTMLurlRequestsList(url2)
+        partial = await fetch(url2)
+          .then(promiseTimeout(timeout))
+          .then(status)
+          .then(text)
+          .then(parse)
+        theTournament.standing.players = theTournament.standing.players.concat(partial.standing.players)
+      }
+
+      // download Results, get Performances
+      let url3 = "https://lichess.org/api/tournament/" + id + "/results"
+      updateHTMLurlRequestsList(url3)
+      let tournamentDone = await fetch(url3)
+        .then(promiseTimeout(timeout))
+        .then(status)
+        .then(text)
+        .then(ndjson2array)
+        .then(parse)
+        .then(function(results) {
+          let performance = new Map();
+          results.forEach(pl => performance.set(pl.username, pl.performance))
+          theTournament.standing.players = theTournament.standing.players.map(pl => { pl.performance = performance.get(pl.name); return pl})
+          return theTournament
+        })
+      return tournamentDone
+    }
+
+    let downloadedTournaments = []
+    while (users.length > 0) {
+      let user = users.shift()
+      let url = "https://lichess.org/api/user/" + user + "/tournament/created";
+      updateHTMLurlRequestsList(url)
+      let missing = await fetch(url)
+        .then(promiseTimeout(timeout))
+        .then(status)
+        .then(text)
+        .then(ndjson2array)
+        .then(parse)
+        .then(getMissingTournaments)
+        .catch(function(error) {
+          console.log('Request failed: ' + url, error);
+        });
+
+      while (missing.length > 0) {
+        let id = missing.pop()
+        let newT = await downloadTournament(id)
+        downloadedTournaments.push(newT)
+      }
+    }
+
+    let tag = document.getElementById("finalJson")
+    tag.innerHTML = JSON.stringify(downloadedTournaments, null, 0);
+
+    return downloadedTournaments
+  }
+
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
