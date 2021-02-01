2a3fead9dedce2c10d7808489f548d1322014ded
Bebul <Bebul@users.noreply.github.com>
2021-02-01 02:07:50 +0100

    Hide Menu on Scroll

--- js/minimal.js
+++ js/minimal.js
@@ -8,3 +8,14 @@ function showMondayFightMenu() {
     x.style.display = "block";
   }
 }
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos != currentScrollPos) {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") x.style.display = "none";
+  }
+  prevScrollpos = currentScrollPos;
+}
