5ef023cb7d9bc8d8e6ee0df2aba15caef03feb94
Bebul <Bebul@users.noreply.github.com>
2021-06-15 17:02:20 +0200

    search engine with possible query q=... in url and queries like date:2021 status:mate loser:jouzolean winner:white

--- js/search.js
+++ js/search.js
@@ -9,11 +9,41 @@ function initSearch() {
 
 function searchGames(fights, tokens) {
   let selectedGames = []
+
+  Number.prototype.padLeft = function(base,chr){
+    var  len = (String(base || 10).length - String(this).length)+1;
+    return len > 0? new Array(len).join(chr || '0')+this : this;
+  }
   fights.forEach( fight => {
     let games = tournamentGames.find(tg => tg.id==fight.id);
     if (games !== undefined) {
       games.games.forEach(game => {
-        let jsonGame = JSON.stringify(game, null, 0).toLowerCase();
+        let date = new Date(0); // The 0 there is the key, which sets the date to the epoch
+        date.setUTCSeconds(game.createdAt/1000);
+        let stringBuilder = []
+        stringBuilder.push(JSON.stringify(game, null, 0))
+
+        let winnerName = ""
+        let loserName = ""
+        if (game.winner==="white") {
+          winnerName = game.players.white.user.name
+          loserName =game.players.black.user.name
+        } else if (game.winner==="black") {
+          winnerName = game.players.black.user.name
+          loserName = game.players.white.user.name
+        }
+        let appended = {
+          date: [date.getFullYear().padLeft(), (date.getMonth()+1).padLeft(), date.getDate().padLeft()].join('/'),
+          winner: winnerName,
+          loser: loserName
+        }
+        stringBuilder.push(
+          ` `,
+          JSON.stringify(appended, null, 0)
+        )
+        let jsonGame = stringBuilder.join("")
+          .toLowerCase()
+          .replace(/"/g, '')
         let found = tokens.find(token => {
           return (jsonGame.indexOf(token) < 0)
         })
@@ -24,8 +54,7 @@ function searchGames(fights, tokens) {
   return {"games": selectedGames}
 }
 
-function processSearch(searchStr) {
-  let theFights = jouzoleanAndBebulsTournaments
+function searchStrintToTokens(searchStr) {
   // RegEx manual:
   //    (?<=y)x ... Lookbehind assertion: Matches "x" only if "x" is preceded by "y". But only x is part of match, the y not.
   //    x(?=y) .... Lookahead assertion: Matches "x" only if "x" is followed by "y". But y is not part of match, only x is.
@@ -42,7 +71,12 @@ function processSearch(searchStr) {
     if (insideQ != null) return insideQ[1]
     else return token
   })
+  return tokens
+}
 
+function processSearch(searchStr) {
+  let theFights = jouzoleanAndBebulsTournaments
+  let tokens = searchStrintToTokens(searchStr)
   let gameData = gameListData(searchGames(theFights, tokens))
 
   updateMostActivePlayer("gameListTable", gameData)
@@ -53,6 +87,8 @@ function processSearch(searchStr) {
 }
 
 function onSearchClicked() {
-  var searchText = document.getElementById("searchInput").value;
+  var searchText = document.getElementById("searchInput").value
+  let newUrl = window.location.pathname + "?q=" + encodeURIComponent(searchText)
+  History.pushState({'q': searchText}, 'Search Engine', newUrl)
   processSearch(searchText)
 }
