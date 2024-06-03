9ffa1681c6856c2dbbe022207ec99ed8bb510d16
Bebul <Bebul@users.noreply.github.com>
2024-06-04 00:56:14 +0200

    new player + 3rd column

--- js/podium.mjs
+++ js/podium.mjs
@@ -1128,7 +1128,8 @@ export var Avatars = function() {
     "travinho", "mates7824", "kasparov", "mates78", "vikjav", "tomasklimecky",
     "dzin69", "janshorny", "arytmik", "tykev123", "pirat77", "puklejchleba", "margarita_vlasenko", "felcar", "droider66",
     "sachycvek", "lastscout", "sumaspandy", "honzahonzahonza", "tomas_1989", "polgu",
-    "barongorc", "michaelchmiel", "tomzr", "tomasklimecky2024", "kamikazeee", "kasparpalov", "dj-strelec", "royalchessyoutube"]
+    "barongorc", "michaelchmiel", "tomzr", "tomasklimecky2024", "kamikazeee", "kasparpalov", "dj-strelec",
+    "royalchessyoutube", "peinsamacze"]
   let useGif = []
   //let useGif = ["bebul", "mrazek", "mozkomor", "jouzolean", "bukowskic", "margarita_vlasenko", "dj-strelec", "neznama-00", "janshorny", "dzin69", "pirat77", "lastscout", "tekele", "vikjav", "mauricedodo", "rychlylenochod", "hrobotron", "felcar", "tomasklimecky", "arytmik", "sachycvek", "travinho", "mates78", "mates7824", "dj-strelec"]
   let defaults = ["default2", "default3"]
